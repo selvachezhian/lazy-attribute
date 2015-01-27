ec6bb0697acb766def9cc0bdc16f4c0c2297b6e0
Selva Chezhian <selvachezhian.labam@gmail.com>
2015-01-27 20:00:02 +0530

    unwanted method removed, code re structured

--- lib/lazy/attribute/lazy_attribute.rb
+++ lib/lazy/attribute/lazy_attribute.rb
@@ -7,42 +7,22 @@ module Lazy
         options.reverse_merge!(default_options)
 
         singleton_class.instance_eval do
-          # options[:key] == :default ? send(:define_method, '[]') : send(:define_method, options[:key]) do |identifier|
-          #   key = find_key(identifier)
-          #
-          #   check_key(key)
-          #
-          #   send(dynamic_find_method(options[:raise_error]), { attr.to_sym => find_identifier(identifier) })
-          #
-          #   # send('find_by', email: 'xyz')
-          # end
 
           if options[:key] == :default
+
             define_method('[]') do |identifier|
-              key = find_key(identifier)
-
-              check_key(key)
-
-              send(dynamic_find_method(options[:raise_error]), { attr.to_sym => find_identifier(identifier) })
+              send_dynamic_method(options, attr, identifier)
             end
 
           else
 
             define_method(options[:key]) do |identifier|
-              key = find_key(identifier)
-
-              check_key(key)
-
-              send(dynamic_find_method(options[:raise_error]), { attr.to_sym => find_identifier(identifier) })
+              send_dynamic_method(options, attr, identifier)
             end
+
           end
 
         end
-      end
-
-      def dynamic_define_method(key)
-        puts key
-        key == :default ? "define_method('[]')" : "define_method(#{key})"
       end
 
       private
@@ -51,16 +31,8 @@ module Lazy
         raise_error ? 'find_by!' : 'find_by'
       end
 
-      def find_key(ident)
-        ident.kind_of?(Array) ? ident.first : :default
-      end
-
-      def check_key(key)
-        raise "Key parameter should be a symbol.  You have passed #{key}" unless key.is_a?(Symbol)
-      end
-
-      def find_identifier(identifier)
-        identifier.kind_of?(Array) ? identifier[1] : identifier
+      def send_dynamic_method(options, attr, identifier)
+        send(dynamic_find_method(options[:raise_error]), { attr.to_sym => identifier })
       end
 
     end

9dc71cd772a31e347ac74106953c085de6c11bb3
Selva Chezhian <selvachezhian.labam@gmail.com>
2015-01-27 15:27:00 +0530

    version 3 Initial Commit

--- lib/lazy/attribute/lazy_attribute.rb
+++ lib/lazy/attribute/lazy_attribute.rb
@@ -3,13 +3,17 @@ module Lazy
     module ClassMethods
 
       def lazy_attribute(attr, options = {})
+        default_options = { raise_error: false }
+
         singleton_class.instance_eval do
           define_method('[]') do |ident|
-            options[:raise_error] ||= false
+            options.reverse_merge!(default_options)
             send(dynamic_find_method(options[:raise_error]), { attr.to_sym => ident })
           end
         end
       end
+
+      private
 
       def dynamic_find_method(raise_error = false)
         raise_error ? 'find_by!' : 'find_by'
--- lib/lazy/attribute/version.rb
+++ lib/lazy/attribute/version.rb
@@ -1,5 +1,5 @@
 module Lazy
   module Attribute
-    VERSION = '2.0.1'
+    VERSION = '3.0.1'
   end
 end

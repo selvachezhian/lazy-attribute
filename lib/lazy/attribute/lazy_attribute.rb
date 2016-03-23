1613925688cc4a5f8de9f15cd5aeda0aacd9eca7
Prasanna <prasannacse2005@gmail.com>
2016-03-23 20:38:14 +0530

    Add docs for better usability

--- lib/lazy/attribute/lazy_attribute.rb
+++ lib/lazy/attribute/lazy_attribute.rb
@@ -1,6 +1,19 @@
 module Lazy
   module Attribute
     module ClassMethods
+
+      # Including the lazy_attribute gem to the model
+      #
+      # @param attribute [Symbol]
+      # @param [Hash] options
+      # @option options [Symbol] :key (:default) if the key parameter is not set, it will take the default as the key
+      # @option options [Boolean] :create_if_not_found (false) if the parameter is set as true, will create the record if the record is not available
+      # @option options [Boolean] :raise_error (false) will raise exceptions
+      #   ActiveRecord::RecordNotFoundException if the value set as true and the record not present <br>
+      #   ActiveRecord::RecordInvalid: Validation failed exception if any validation error thrown at the time of creation missing record
+      #
+      # @return [none]
+
       def lazy_attribute(attribute, options = {})
         default_options = { raise_error: false, key: [], create_if_not_found: false }
         options.reverse_merge!(default_options)
